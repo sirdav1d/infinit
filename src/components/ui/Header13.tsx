2f67ebc44057875c6c3bb9cd0bf59237b2b1da7a
sirdav1d <ddavid.diniz@gmail.com>
2024-09-15 10:43:31 -0300

    feat: :lipstick: ajustes de alinhamento

--- src/components/ui/Header13.tsx
+++ src/components/ui/Header13.tsx
@@ -18,7 +18,7 @@ export const Header13 = (props: Header13SlotProps) => {
 		...props,
 	} as Props;
 	return (
-		<section className='flex min-h-screen h-full items-center justify-between md:justify-start gap-5 xxl:gap-8 flex-col shadow-small rounded-3xl'>
+		<section className='flex min-h-screen h-full items-center justify-center md:justify-start gap-5 xxl:gap-8 flex-col shadow-small rounded-3xl'>
 			<div className='hidden md:flex relative w-full  h-[64vh] xxl:h-[80vh] md:flex-0 bg-zinc-950/20 flex-0'>
 				<div className=' absolute w-full top-0 left-0 h-full -z-10'>
 					<video
@@ -39,7 +39,7 @@ export const Header13 = (props: Header13SlotProps) => {
 				<div className='container'>
 					<div className='grid grid-rows-1 mt-60 md:mt-0 items-start gap-2 pb-12 md:grid-cols-2 md:gap-x-12 md:gap-y-8 lg:gap-x-20 lg:gap-y-16 '>
 						<div>
-							<Heading className='text-3xl md:text-5xl xl:text-6xl font-bold capitalize'>
+							<Heading className='text-4xl md:text-5xl xl:text-6xl font-bold capitalize'>
 								{slot1}
 							</Heading>
 						</div>
