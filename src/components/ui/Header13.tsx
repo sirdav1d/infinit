f56db713197f34e621ddf6bcaeb53b4cfdc8cbcf
sirdav1d <ddavid.diniz@gmail.com>
2024-09-18 04:39:08 -0300

    fix: :lipstick: ajuste de video para mobile

--- src/components/ui/Header13.tsx
+++ src/components/ui/Header13.tsx
@@ -18,8 +18,8 @@ export const Header13 = (props: Header13SlotProps) => {
 		...props,
 	} as Props;
 	return (
-		<section className='flex h-screen md:h-full items-center justify-start gap-5 flex-col'>
-			<div className='flex md:flex relative w-full  h-[60vh] xxl:h-[67vh] bg-zinc-950/10 flex-0'>
+		<section className='flex h-screen md:h-full items-center justify-center md:justify-start gap-5 flex-col'>
+			<div className='hidden md:flex relative w-full  h-[60vh] xxl:h-[67vh] bg-zinc-950/10 flex-0'>
 				<div className=' absolute w-full top-0 left-0 h-full -z-10'>
 					<video
 						className='object-cover object-center w-full h-[60vh] xxl:h-[67vh] '
@@ -39,7 +39,7 @@ export const Header13 = (props: Header13SlotProps) => {
 				<div className='container'>
 					<div className='grid grid-rows-1 md:mt-0 items-start gap-2 pb-12 md:grid-cols-2 md:gap-x-12 md:gap-y-8 lg:gap-x-20 lg:gap-y-16'>
 						<div>
-							<Heading className='text-4xl md:text-5xl xxl:text-6xl font-bold capitalize'>
+							<Heading className='text-5xl xxl:text-6xl font-bold capitalize'>
 								{slot1}
 							</Heading>
 						</div>
